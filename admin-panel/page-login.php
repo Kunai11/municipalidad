5ca7ca2ea9c5712c821fc4139f380040dae17e60
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-08-07 17:30:40 -0600

    Cambiar las direcciones a los iconos locales

--- admin-panel/page-login.php
+++ admin-panel/page-login.php
@@ -7,7 +7,7 @@
     <!-- CSS-->
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <!-- Font-icon css-->
-    <!--<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">-->
+    <!--<link rel="stylesheet" type="text/css" href="css/font-awesome-4.7.0/css/font-awesome.min.css">-->
     <title>Admin Panel</title>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries-->
     <!--if lt IE 9
