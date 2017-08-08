7b5e0685b7aeab07049827222ff4a0776358c26a
Kunai11 <carlomeza11.cm@gmail.com>
2017-08-08 17:49:06 -0600

    Modulo agrgar empleado (Incompleto)

--- admin-panel/agregar_empleado.php
+++ admin-panel/agregar_empleado.php
@@ -2,12 +2,12 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSS-->
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <!-- Font-icon css-->
-    <!--<link rel="stylesheet" type="text/css" href="css/font-awesome-4.7.0/css/font-awesome.min.css">-->
     <link rel="stylesheet" type="text/css" href="css/font-awesome-4.7.0/css/font-awesome.min.css">
     <title>Admin Panel</title>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries-->
@@ -19,14 +19,14 @@
   <body class="sidebar-mini fixed">
     <div class="wrapper">
       <!-- Navbar-->
-      <header class="main-header hidden-print"><a class="logo" href="index.html">Admin Panel</a>
+      <header class="main-header hidden-print"><a class="logo" href="index.php">Admin Panel</a>
         <nav class="navbar navbar-static-top">
           <!-- Sidebar toggle button--><a class="sidebar-toggle" href="#" data-toggle="offcanvas"></a>
           <!-- Navbar Right Menu-->
           <div class="navbar-custom-menu">
             <ul class="top-nav">
               <!--Notification Menu-->
-              <!--<li class="dropdown notification-menu"><a class="dropdown-toggle" href="#" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-bell-o fa-lg"></i></a>
+              <li class="dropdown notification-menu"><a class="dropdown-toggle" href="#" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-bell-o fa-lg"></i></a>
                 <ul class="dropdown-menu">
                   <li class="not-head">You have 4 new notifications.</li>
                   <li><a class="media" href="javascript:;"><span class="media-left media-icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-primary"></i><i class="fa fa-envelope fa-stack-1x fa-inverse"></i></span></span>
@@ -37,13 +37,13 @@
                       <div class="media-body"><span class="block">Transaction xyz complete</span><span class="text-muted block">2min ago</span></div></a></li>
                   <li class="not-footer"><a href="#">See all notifications.</a></li>
                 </ul>
-              </li>-->
+              </li>
               <!-- User Menu-->
               <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user fa-lg"></i></a>
                 <ul class="dropdown-menu settings-menu">
                   <li><a href="page-user.html"><i class="fa fa-cog fa-lg"></i> Settings</a></li>
                   <li><a href="page-user.html"><i class="fa fa-user fa-lg"></i> Profile</a></li>
-                  <li><a href="page-login.php"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
+                  <li><a href="page-login.html"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
                 </ul>
               </li>
             </ul>
@@ -54,7 +54,7 @@
       <aside class="main-sidebar hidden-print">
         <section class="sidebar">
           <div class="user-panel">
-            <div class="pull-left image"><img class="img-circle" src="images/user.png" alt="User Image"></div>
+            <div class="pull-left image"><img class="img-circle" src="https://s3.amazonaws.com/uifaces/faces/twitter/jsa/48.jpg" alt="User Image"></div>
             <div class="pull-left info">
               <p>John Doe</p>
               <p class="designation">Frontend Developer</p>
@@ -62,34 +62,15 @@
           </div>
           <!-- Sidebar Menu-->
           <ul class="sidebar-menu">
-            <li class="active"><a href="index.html"><i class="fa fa-dashboard"></i><span>Inicio</span></a></li>
-            <li class="treeview"><a href="#"><i class="fa fa-laptop"></i><span>Gestion de empleados</span><i class="fa fa-angle-right"></i></a>
+            <li class="active"><a href="index.php"><i class="fa fa-dashboard"></i><span>Inicio</span></a></li>
+            <li class="treeview"><a href="#"><i class="fa fa-laptop"></i><span>UI Elements</span><i class="fa fa-angle-right"></i></a>
               <ul class="treeview-menu">
-                <li><a href="agregar_empleado.php"><i class="fa fa-circle-o"></i> Agregar nuevo empleado</a></li>
-                <li><a href="http://fontawesome.io/icons/" target="_blank"><i class="fa fa-circle-o"></i> Modificar datos de empleado</a></li>
-                <li><a href="ui-cards.html"><i class="fa fa-circle-o"></i> Eliminar datos de empleado</a></li>
-                <li><a href="widgets.html"><i class="fa fa-circle-o"></i> Mostrar lista de empleados</a></li>
-                <li><a href="#"><i class="fa fa-circle-o"></i> Consultar hoja de vida</a></li>
+                <li><a href="bootstrap-components.html"><i class="fa fa-circle-o"></i> Bootstrap Elements</a></li>
+                <li><a href="http://fontawesome.io/icons/" target="_blank"><i class="fa fa-circle-o"></i> Font Icons</a></li>
+                <li><a href="ui-cards.html"><i class="fa fa-circle-o"></i> Cards</a></li>
+                <li><a href="widgets.html"><i class="fa fa-circle-o"></i> Widgets</a></li>
               </ul>
             </li>
-               
-            <li class="treeview"><a href="#"><i class="fa fa-edit"></i><span>Departamentos</span><i class="fa fa-angle-right"></i></a>
-              <ul class="treeview-menu">
-                <li><a href=""><i class="fa fa-circle-o"></i> Agregar departamento</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Modificar departamento</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Eliminar deparatamento</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Mostrar departamentos</a></li>
-              </ul>
-            </li>
-
-              <li class="treeview"><a href="#"><i class="fa fa-edit"></i><span>Cargos</span><i class="fa fa-angle-right"></i></a>
-              <ul class="treeview-menu">
-                <li><a href=""><i class="fa fa-circle-o"></i> Crear y asignar cargo</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Modificar cargo</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Eliminar cargo</a></li>
-              </ul>
-            </li>
-
             <li><a href="charts.html"><i class="fa fa-pie-chart"></i><span>Charts</span></a></li>
             <li class="treeview"><a href="#"><i class="fa fa-edit"></i><span>Forms</span><i class="fa fa-angle-right"></i></a>
               <ul class="treeview-menu">
@@ -108,7 +89,7 @@
             <li class="treeview"><a href="#"><i class="fa fa-file-text"></i><span>Pages</span><i class="fa fa-angle-right"></i></a>
               <ul class="treeview-menu">
                 <li><a href="blank-page.php"><i class="fa fa-circle-o"></i> Blank Page</a></li>
-                <li><a href="page-login.php"><i class="fa fa-circle-o"></i> Login Page</a></li>
+                <li><a href="page-login.html"><i class="fa fa-circle-o"></i> Login Page</a></li>
                 <li><a href="page-lockscreen.html"><i class="fa fa-circle-o"></i> Lockscreen Page</a></li>
                 <li><a href="page-user.html"><i class="fa fa-circle-o"></i> User Page</a></li>
                 <li><a href="page-invoice.html"><i class="fa fa-circle-o"></i> Invoice Page</a></li>
@@ -134,32 +115,104 @@
       <div class="content-wrapper">
         <div class="page-title">
           <div>
-            <h1><i class="fa fa-dashboard"></i> Dashboard</h1>
-            <p>A free and modular admin template</p>
+            <h1><i class="fa fa-edit"></i> Form Samples</h1>
+            <p>Sample forms</p>
           </div>
           <div>
             <ul class="breadcrumb">
               <li><i class="fa fa-home fa-lg"></i></li>
-              <li><a href="#">Inicio</a></li>
+              <li>Forms</li>
+              <li><a href="#">Sample Forms</a></li>
             </ul>
           </div>
         </div>
         <div class="row">
           <div class="col-md-6">
-            <div class="card">
-              <h3 class="card-title">Getting Started</h3>
-              <p>Vali is a free and responsive dashboard theme built with Bootstrap, Pug.js (templating) and SASS. It's fully customizable and modular. You don't need to add the code, you will not use.</p>
-              <p>The issue with the most admin themes out there is that if you will see their source code there are a hell lot of external CSS and javascript files in there. And if you try to remove a CSS or Javascript file some things stops working.</p>
-              <p>That's why I made Vali. Which is a light weight yet expendable and good looking theme. The theme has all the features required in a dashboard theme but this features are built like plug and play module. Take a look at the <a href="http://pratikborsadiya.in/blog/vali-admin" target="_blank">documentation</a> about customizing the theme.</p>
-              <p class="mt-40 mb-20"><a class="btn btn-primary icon-btn mr-10" href="http://pratikborsadiya.in/blog/vali-admin" target="_blank"><i class="fa fa-file"></i>Docs</a><a class="btn btn-info icon-btn mr-10" href="https://github.com/pratikborsadiya/vali-admin" target="_blank"><i class="fa fa-github"></i>GitHub</a><a class="btn btn-success icon-btn" href="https://github.com/pratikborsadiya/vali-admin/archive/master.zip" target="_blank"><i class="fa fa-download"></i>Download</a></p>
+            <div class="registro" >
+              <h3 class="card-title">Registro de empleado</h3>
+              <div class="card-body">
+                <form>
+                
+
+                  <div class="form-group" id="identidad">
+                    <label class="control-label">Numero de Identidad</label>
+                    <input class="input_empleado" type="text" placeholder="Introducir numero de identidad" required>
+                  </div>
+
+	                <div class="form-group" id="nombre">
+                    <label class="control-label">Nombre</label>
+                    <input class="input_empleado" type="text" placeholder="Introducir nombre" required>
+                  </div>
+                 
+
+                  <div class="form-group" id="nombre">
+                    <label class="control-label">Primer Apellido</label>
+                    <input class="input_empleado" type="text" placeholder="" required>
+                  </div>
+
+                  <div class="form-group" id="nombre">
+                    <label class="control-label">Segundo Apellido</label>
+                    <input class="input_empleado" type="text" placeholder="" required>
+                  </div>
+
+                  <div class="form-group" id="nombre">
+                    <label class="control-label">Lugar de Nacimiento</label>
+                    <input class="input_empleado" type="text" placeholder="" required>
+                  </div>
+
+                  <div class="form-group" id="nombre">
+                    <label class="control-label">Fecha de Nacimiento</label>
+                    <input class="input_empleado" type="date"  required>
+                  </div>
+
+                  <div class="form-group">
+                    <label class="control-label">Email</label>
+                    <input class="input_empleado" type="email" placeholder="Enter email address">
+                  </div>
+                  <div class="form-group">
+                    <label class="control-label">Address</label>
+                    <textarea class="input_empleado" rows="4" placeholder="Enter your address"></textarea>
+                  </div>
+                  <div class="form-group">
+                    <label class="control-label">Gender</label>
+                    <div class="radio">
+                      <label>
+                        <input type="radio" name="gender">Male
+                      </label>
+                    </div>
+                    <div class="radio">
+                      <label>
+                        <input type="radio" name="gender">Female
+                      </label>
+                    </div>
+                  </div>
+                  
+                  <div class="form-group">
+                    
+                  </div>
+                </form>
+              </div>
+              <div class="card-footer">
+                <button class="btn btn-primary icon-btn" type="button"><i class="fa fa-fw fa-lg fa-check-circle"></i>Register</button>&nbsp;&nbsp;&nbsp;<a class="btn btn-default icon-btn" href="#"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cancel</a>
+              </div>
             </div>
           </div>
-          <div class="col-md-6">
-            <div class="card">
-              <h3 class="card-title">Compatibility with frameworks</h3>
-              <p>This theme is not built for a specific framework or technology like Angular or React etc. But due to it's modular nature it's very easy to incorporate it into any front-end or back-end framework like Angular, React or Laravel.</p>
-              <p>Go to <a href="http://pratikborsadiya.in/blog/vali-admin" target="_blank">documentation</a> for more details about integrating this theme with various frameworks.</p>
-              <p>The source code is available on GitHub. If anything is missing or weird please report it as an issue on <a href="https://github.com/pratikborsadiya/vali-admin" target="_blank">GitHub</a>. If you want to contribute to this theme pull requests are always welcome.</p>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <div class="card-footer">
+                <div class="row">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="clearix"></div>
+          <div class="col-md-12">
+            
+                </form>
+              </div>
             </div>
           </div>
         </div>
