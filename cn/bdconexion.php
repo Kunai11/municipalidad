652d112b7d1f8c347936d389485b63c8320c1f2a
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-08-09 10:08:13 -0600

    Cambio de nombre de DB

--- cn/bdconexion.php
+++ cn/bdconexion.php
@@ -1,6 +1,6 @@
 <?php
 $hostname_cn = "localhost";
-$database_cn = "inventario_1.2";
+$database_cn = "municipalidad_bd_2.0";
 $username_cn = "root";
 $password_cn = "";
 $db = mysqli_connect($hostname_cn,$username_cn,$password_cn,$database_cn);
