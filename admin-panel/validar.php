cb7d76835b0902bed33be3d4a2508a67e6189a4c
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-08-09 11:37:29 -0600

    Finalizar Validacion de login para usuario existente

--- admin-panel/validar.php
+++ admin-panel/validar.php
@@ -16,8 +16,8 @@
         } else {
             $queryUsuario=mysqli_query($db, "SELECT * FROM usuarios WHERE Username='$usuario' AND Pass='$pass'") or die(mysqli_error);
             if ($rowQueryUsuario = mysqli_fetch_array($queryUsuario)) {
-                $Id_Usuario = $rowQueryUsuario['Id_Usuario'] ;
-                $queryDatosEmp=mysqli_query($db, "SELECT Dep.*, Car.*, Emp.* FROM departamentos Dep INNER JOIN(cargos Car INNER JOIN(empleados Emp INNER JOIN asignaciones Asig ON Emp.Id_Empleado=Asig.Id_Empleado) ON Car.Cod_Cargo=Asig.Cod_Cargo) ON Dep.Cod_Dep=Asig.Cod_Dep WHERE Emp.Id_Empleado = '$Id_Usuario'") or die(mysqli_error);
+                $Id_Empleado = $rowQueryUsuario['Id_Empleado'] ;
+                $queryDatosEmp=mysqli_query($db, "SELECT Dep.*, Car.*, Emp.* FROM departamentos Dep INNER JOIN(cargos Car INNER JOIN(empleados Emp INNER JOIN asignaciones Asig ON Emp.Id_Empleado=Asig.Id_Empleado) ON Car.Cod_Cargo=Asig.Cod_Cargo) ON Dep.Cod_Dep=Asig.Cod_Dep WHERE Emp.Id_Empleado = '$Id_Empleado'") or die(mysqli_error);
                 if ($rowQueryDatosEmp = mysqli_fetch_array($queryDatosEmp)) {
                     $_SESSION['codigoUsuario'] = $rowQueryUsuario["Cod_Usuario"];
                     $_SESSION['username'] = $rowQueryUsuario["Username"];
@@ -27,6 +27,11 @@
                     $_SESSION['username'] = $rowQueryDatosEmp["Nombres"] . " " . $rowQueryDatosEmp["Apellido1"];
                     $_SESSION['rank'] = $rowQueryDatosEmp["Nom_Cargo"] . " de " . $rowQueryDatosEmp["Nom_Dep"];
                     echo '<p class="semibold-text mb-0" style="color:green;text-align:center;">Acceso concedido</p>';
+                    ?>
+                    <script language="javascript"> 
+                        window.setTimeout('location.href="index.php"', 2000);
+                    </script>
+                    <?php
                 }
             } else {
                 echo '<p class="mb-0" style="color:red;text-align:center;">Usuario o contrase&ntilde;a incorrectos</p>';
