8bf018fb81559ceea8fe68e651b43b40c385eb4d
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-08-08 20:03:17 -0600

    Reordenar Menu, Cambiar iconos

--- admin-panel/index.php
+++ admin-panel/index.php
@@ -39,11 +39,11 @@
                 </ul>
               </li>-->
               <!-- User Menu-->
-              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user fa-lg"></i></a>
+              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user-circle fa-lg"></i></a>
                 <ul class="dropdown-menu settings-menu">
-                  <li><a href="page-user.html"><i class="fa fa-cog fa-lg"></i> Settings</a></li>
-                  <li><a href="page-user.html"><i class="fa fa-user fa-lg"></i> Profile</a></li>
-                  <li><a href="page-login.php"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
+                  <!--<li><a href="page-user.html"><i class="fa fa-cog fa-lg"></i> Settings</a></li>-->
+                  <li><a href="page-user.html"><i class="fa fa-user fa-lg"></i> Editar Perfil</a></li>
+                  <li><a href="page-login.php"><i class="fa fa-sign-out fa-lg"></i> Cerrar Sesi&oacute;n</a></li>
                 </ul>
               </li>
             </ul>
@@ -63,85 +63,83 @@
 
           <!-- Sidebar Menu-->
           <ul class="sidebar-menu">
-            <li class="active"><a href="index.html"><i class="fa fa-dashboard"></i><span>Inicio</span></a></li>
-            <li class="treeview"><a href="#"><i class="fa fa-laptop"></i><span>Gestiones administrativas</span><i class="fa fa-angle-right"></i></a>
-              <ul class="treeview-menu">
+            <li class="active"><a href="index.php"><i class="fa fa-home"></i><span>Inicio</span></a></li>
 
-            <li class="treeview"><a href="#"><i class="fa fa-laptop"></i><span>Gestion de empleados</span><i class="fa fa-angle-right"></i></a>
-              <ul class="treeview-menu">
-                <li><a href="agregar_empleado.php"><i class="fa fa-circle-o"></i> Agregar nuevo empleado</a></li>
-                <li><a href="http://fontawesome.io/icons/" target="_blank"><i class="fa fa-circle-o"></i> Modificar datos de empleado</a></li>
-                <li><a href="ui-cards.html"><i class="fa fa-circle-o"></i> Eliminar datos de empleado</a></li>
+            <li class="treeview"><a href="#"><i class="fa fa-institution"></i><span>Opciones Administrativas</span><i class="fa fa-angle-right"></i></a>
+            <ul class="treeview-menu">
+              <li class="treeview"><a href="#"><i class="fa fa-group"></i><span>Empleados</span><i class="fa fa-angle-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href="agregar_empleado.php"><i class="fa fa-plus-square"></i> Agregar nuevo</a></li>
+                  <li><a href="#" target="_blank"><i class="fa fa-pencil-square-o"></i> Modificar existente</a></li>
+                  <li><a href="#"><i class="fa fa-minus-square"></i> Eliminar existente</a></li>
+                </ul>
+              </li>
                 
-              </ul>
-            </li>
-               
-            <li class="treeview"><a href="#"><i class="fa fa-share"></i><span>Departamentos</span><i class="fa fa-angle-right"></i></a>
-              <ul class="treeview-menu">
-                <li><a href=""><i class="fa fa-circle-o"></i> Agregar departamento</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Modificar departamento</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Eliminar deparatamento</a></li>
-               
-              </ul>
-            </li>
+              <li class="treeview"><a href="#"><i class="fa fa-suitcase"></i><span>Departamentos</span><i class="fa fa-angle-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href=""><i class="fa fa-plus-square"></i> Agregar nuevo</a></li>
+                  <li><a href=""><i class="fa fa-pencil-square-o"></i> Modificar existente</a></li>
+                  <li><a href=""><i class="fa fa-minus-square"></i> Eliminar existente</a></li>
+                </ul>
+              </li>
 
-            <li class="treeview"><a href="#"><i class="fa fa-share"></i><span>Cargos</span><i class="fa fa-angle-right"></i></a>
-              <ul class="treeview-menu">
-                <li><a href=""><i class="fa fa-circle-o"></i> Crear y asignar cargo</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Modificar cargo</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Eliminar cargo</a></li>
-              </ul>
-            </li>
-</ul>
-               <li class="treeview"><a href="#"><i class="fa fa-edit"></i><span>Usuario</span><i class="fa fa-angle-right"></i></a>
-              <ul class="treeview-menu">
-                <li><a href=""><i class="fa fa-circle-o"></i> Crear Usuario</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Modificar Usuario</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Eliminar Usuario</a></li>
-              </ul>
-            </li>
-
-            <li class="treeview"><a href="#"><i class="fa fa-share"></i><span>Opciones financieras</span><i class="fa fa-angle-right"></i></a>
-              <ul class="treeview-menu">
-                 <li class="treeview"><a href="#"><i class="fa fa-share"></i><span>Pagos</span><i class="fa fa-angle-right"></i></a>
-                    <ul class="treeview-menu">
-                <li><a href=""><i class="fa fa-circle-o"></i> Nuevo pago</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Modificar pago</a></li>
-              </ul>
+              <li class="treeview"><a href="#"><i class="fa fa-id-card-o"></i><span>Cargos</span><i class="fa fa-angle-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href=""><i class="fa fa-plus-square"></i> Crear y asignar cargo</a></li>
+                  <li><a href=""><i class="fa fa-pencil-square-o"></i> Modificar existente</a></li>
+                  <li><a href=""><i class="fa fa-minus-square"></i> Eliminar existente</a></li>
+                </ul>
+              </li>
             </ul>
-            </li>
 
-            <li class="treeview"><a href="#"><i class="fa fa-share"></i><span>Registro</span><i class="fa fa-angle-right"></i></a>
+            <li class="treeview"><a href="#"><i class="fa fa-money"></i><span>Opciones Financieras</span><i class="fa fa-angle-right"></i></a>
               <ul class="treeview-menu">
-                <li><a href=""><i class="fa fa-circle-o"></i> Historial</a></li>
+                <li class="treeview"><a href="#"><i class="fa fa-dollar"></i><span>Pagos</span><i class="fa fa-angle-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href=""><i class="fa fa-credit-card"></i> Nuevo pago</a></li>
+                  <li><a href=""><i class="fa fa-pencil-square-o"></i> Modificar pago</a></li>
+                </ul>
+                <li class="treeview"><a href="#"><i class="fa fa-clipboard"></i><span>Registro</span><i class="fa fa-angle-right"></i></a>
+                  <ul class="treeview-menu">
+                    <li><a href=""><i class="fa fa-history"></i> Historial</a></li>
+                  </ul>
+                </li>
               </ul>
             </li>
 
-            <li class="treeview"><a href="#"><i class="fa fa-share"></i><span>Reportes</span><i class="fa fa-angle-right"></i></a>
+            <li class="treeview"><a href="#"><i class="fa fa-user-circle"></i><span>Gesti&oacute;n de Usuarios</span><i class="fa fa-angle-right"></i></a>
               <ul class="treeview-menu">
-                <li><a href=""><i class="fa fa-circle-o"></i> Listado Departamentos</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Listado de Cargos</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Listado de Empleados</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Listado de Usuarios</a></li>
+                <li><a href=""><i class="fa fa-user-plus"></i> Crear Usuario</a></li>
+                <li><a href=""><i class="fa fa-pencil-square-o"></i> Modificar Usuario</a></li>
+                <li><a href=""><i class="fa fa-user-times"></i> Eliminar Usuario</a></li>
               </ul>
             </li>
 
-            <li class="treeview"><a href="#"><i class="fa fa-share"></i><span>Busqueda</span><i class="fa fa-angle-right"></i></a>
+            <li class="treeview"><a href="#"><i class="fa fa-clipboard"></i><span>Reportes</span><i class="fa fa-angle-right"></i></a>
               <ul class="treeview-menu">
-                <li><a href=""><i class="fa fa-circle-o"></i> Buscar departamento</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Buscar Cargo</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Buscar empleado</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Buscar usuario</a></li>
+                <li><a href=""><i class="fa fa-file-text-o"></i> Listado de Departamentos</a></li>
+                <li><a href=""><i class="fa fa-file-text-o"></i> Listado de Cargos</a></li>
+                <li><a href=""><i class="fa fa-file-text-o"></i> Listado de Empleados</a></li>
+                <li><a href=""><i class="fa fa-file-text-o"></i> Listado de Usuarios</a></li>
               </ul>
             </li>
 
-             <li class="treeview"><a href="#"><i class="fa fa-share"></i><span>Configuraci&oacute;n</span><i class="fa fa-angle-right"></i></a>
+            <li class="treeview"><a href="#"><i class="fa fa-search"></i><span>Busqueda</span><i class="fa fa-angle-right"></i></a>
               <ul class="treeview-menu">
-                 <li class="treeview"><a href="#"><i class="fa fa-share"></i><span>Planilla predeterminada</span><i class="fa fa-angle-right"></i></a>
+                <li><a href=""><i class="fa fa-search"></i> Buscar Departamento</a></li>
+                <li><a href=""><i class="fa fa-search"></i> Buscar Cargo</a></li>
+                <li><a href=""><i class="fa fa-search"></i> Buscar Empleado</a></li>
+                <li><a href=""><i class="fa fa-search"></i> Buscar Usuario</a></li>
+              </ul>
+            </li>
+
+             <li class="treeview"><a href="#"><i class="fa fa-gears"></i><span>Configuraci&oacute;n</span><i class="fa fa-angle-right"></i></a>
+              <ul class="treeview-menu">
+                 <li class="treeview"><a href="#"><i class="fa fa-wpforms"></i><span>Planillas predefinidas</span><i class="fa fa-angle-right"></i></a>
                     <ul class="treeview-menu">
-                <li><a href=""><i class="fa fa-circle-o"></i> Crear planilla</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Modificar planilla</a></li>
-                <li><a href=""><i class="fa fa-circle-o"></i> Eliminar planilla</a></li>
+                <li><a href=""><i class="fa fa-plus-square"></i> Crear nueva</a></li>
+                <li><a href=""><i class="fa fa-pencil-square-o"></i> Modificar existente</a></li>
+                <li><a href=""><i class="fa fa-minus-square"></i> Eliminar existente</a></li>
               </ul>
             </ul>
             </li>
@@ -191,8 +189,8 @@
       <div class="content-wrapper">
         <div class="page-title">
           <div>
-            <h1><i class="fa fa-dashboard"></i> Dashboard</h1>
-            <p>A free and modular admin template</p>
+            <h1><i class="fa fa-home"></i> Inicio</h1>
+            <p>Pantalla de inicio de Admin Panel</p>
           </div>
           <div>
             <ul class="breadcrumb">
