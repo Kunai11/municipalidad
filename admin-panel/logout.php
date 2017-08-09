f7e8c114c2399ae299a94ed839583311ae717ead
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-08-09 11:43:08 -0600

    Validacion extra

--- admin-panel/logout.php
+++ admin-panel/logout.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     include('../cn/bdconexion.php');
-    if (isset($_SESSION['username'])) {
+    if (isset($_SESSION['username'])&&($_SESSION['title'])) {
         session_destroy();
         header('Location: page-login.php');
     }else {
