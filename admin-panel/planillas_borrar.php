8a1e81b113ec4afcebde5e57274f2404880a88c6
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-08-12 01:14:14 -0600

    Codigo PHP que verifica existencia de  registro y elimina en caso verdadero

---
+++ admin-panel/planillas_borrar.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+    if (isset($_SESSION['username'])&&($_SESSION['rank'])) {
+
+        include('../cn/bdconexion.php');
+        $codigo = $_POST['codigo_planilla'];
+
+        $queryVerificar = mysqli_query($db, "SELECT COUNT(*) as Existe FROM planillas WHERE Cod_Planilla='$codigo'") or die(mysqli_error());
+        $rowExiste=mysqli_fetch_array($queryVerificar);
+        if ($rowExiste['Existe']==1) {
+            $queryBorrar = mysqli_query($db, "DELETE FROM planillas WHERE Cod_Planilla='$codigo'") or die(mysqli_error());
+            echo 'Borrado';
+        }
+        if ($rowExiste['Existe']==0) {
+            #echo 'No existe';
+        } 
+        
+            
+    } else {
+        header('location: page_denegado.php');
+    }
+?>
