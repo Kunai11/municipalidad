8bf8c8d334c0882ca66d061e0c2c6b91edaae750
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-08-11 23:01:59 -0600

    Optimizar Codigo

--- admin-panel/planillas_guardar.php
+++ admin-panel/planillas_guardar.php
@@ -1,56 +1,25 @@
 <?php
     session_start();
     if (isset($_SESSION['username'])&&($_SESSION['rank'])) {
-        
-        #function agregarPlanilla(){
-            include('../cn/bdconexion.php');
-            $codigo = $_POST['codigo_planilla'];
-            $desc = $_POST['descripcion_planilla'];
-            $tipo = $_POST['tipo_planilla'];
-            $sueldoBase = $_POST['sueldo_base'];
-            $deducIHSS = $_POST['deduc_IHSS'];
-            $deducEsp = $_POST['deduc_Esp'];
-            $sueldoNeto = $_POST['sueldo_neto'];
 
-            $queryVerificar = mysqli_query($db, "SELECT COUNT(*) as Existe FROM planillas WHERE Tipo='".$tipo."'") or die(mysqli_error());
-            $rowExiste=mysqli_fetch_array($queryVerificar);
-            if ($rowExiste['Existe']==0) {
-                $queryGuardar = mysqli_query($db, "INSERT INTO planillas (Cod_Planilla, Descripcion, Tipo, Sueldo_Base, Ded_IHSS, Ded_Especiales, Salario_Neto) VALUES ('$codigo', '$desc', '$tipo', '$sueldoBase', '$deducIHSS', '$deducEsp', '$sueldoNeto') ") or die(mysqli_error());
-                echo 'Guardado';
-                #echo '<input type="hidden" id="respuesta" value="1">';
-                #header("location: planillas_listado_completo.php");
-            } 
-            if ($rowExiste['Existe']==1) {
-                #echo '<input type="hidden" id="respuesta" value="0">';
-                #echo 'Ya existe';
-            }
-            
-        #}
+        include('../cn/bdconexion.php');
+        $codigo = $_POST['codigo_planilla'];
+        $desc = $_POST['descripcion_planilla'];
+        $tipo = $_POST['tipo_planilla'];
+        $sueldoBase = $_POST['sueldo_base'];
+        $deducIHSS = $_POST['deduc_IHSS'];
+        $deducEsp = $_POST['deduc_Esp'];
+        $sueldoNeto = $_POST['sueldo_neto'];
 
-        // function modificarPlanilla(){
-        //     include('../cn/bdconexion.php');
-        //     $codigo = $_SESSION['codigo_planilla'];
-        //     $desc = $_POST['descripcion_planilla'];
-        //     $tipo = $_POST['tipo_planilla'];
-        //     $sueldoBase = $_POST['sueldo_base'];
-        //     $deducIHSS = $_POST['deduc_IHSS'];
-        //     $deducEsp = $_POST['deduc_Esp'];
-        //     $sueldoNeto = $_POST['sueldo_neto'];
-        //     $queryCambiarUser = mysqli_query($db, "UPDATE usuarios SET Username='$userName', Pass='$newPass' WHERE Cod_Usuario='".$_SESSION['codigoUsuario']."'") or die(mysqli_error());
-        // }
-
-        // function eliminarPlanilla(){
-        //     include('../cn/bdconexion.php');
-        //     $codigo = $_SESSION['codigo_planilla'];
-        //     $desc = $_POST['descripcion_planilla'];
-        //     $tipo = $_POST['tipo_planilla'];
-        //     $sueldoBase = $_POST['sueldo_base'];
-        //     $deducIHSS = $_POST['deduc_IHSS'];
-        //     $deducEsp = $_POST['deduc_Esp'];
-        //     $sueldoNeto = $_POST['sueldo_neto'];
-
-        // }
-
+        $queryVerificar = mysqli_query($db, "SELECT COUNT(*) as Existe FROM planillas WHERE Tipo='".$tipo."'") or die(mysqli_error());
+        $rowExiste=mysqli_fetch_array($queryVerificar);
+        if ($rowExiste['Existe']==0) {
+            $queryGuardar = mysqli_query($db, "INSERT INTO planillas (Cod_Planilla, Descripcion, Tipo, Sueldo_Base, Ded_IHSS, Ded_Especiales, Salario_Neto) VALUES ('$codigo', '$desc', '$tipo', '$sueldoBase', '$deducIHSS', '$deducEsp', '$sueldoNeto') ") or die(mysqli_error());
+            echo 'Guardado';
+        } 
+        if ($rowExiste['Existe']==1) {
+            #echo 'Ya existe';
+        }
             
     } else {
         header('location: page_denegado.php');
