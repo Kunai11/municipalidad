9894f62ea416a1279d8d6f7800bf121850c4b971
CarlosDavidVasquez <carlosdavidvasquez7@gmail.com>
2017-08-13 01:25:33 -0600

    Agregar informacion para el usuario

--- admin-panel/index.php
+++ admin-panel/index.php
@@ -137,19 +137,27 @@
         <div class="row">
           <div class="col-md-6">
             <div class="card">
-              <h3 class="card-title">Getting Started</h3>
-              <p>Vali is a free and responsive dashboard theme built with Bootstrap, Pug.js (templating) and SASS. It's fully customizable and modular. You don't need to add the code, you will not use.</p>
-              <p>The issue with the most admin themes out there is that if you will see their source code there are a hell lot of external CSS and javascript files in there. And if you try to remove a CSS or Javascript file some things stops working.</p>
-              <p>That's why I made Vali. Which is a light weight yet expendable and good looking theme. The theme has all the features required in a dashboard theme but this features are built like plug and play module. Take a look at the <a href="http://pratikborsadiya.in/blog/vali-admin" target="_blank">documentation</a> about customizing the theme.</p>
-              <p class="mt-40 mb-20"><a class="btn btn-primary icon-btn mr-10" href="http://pratikborsadiya.in/blog/vali-admin" target="_blank"><i class="fa fa-file"></i>Docs</a><a class="btn btn-info icon-btn mr-10" href="https://github.com/pratikborsadiya/vali-admin" target="_blank"><i class="fa fa-github"></i>GitHub</a><a class="btn btn-success icon-btn" href="https://github.com/pratikborsadiya/vali-admin/archive/master.zip" target="_blank"><i class="fa fa-download"></i>Download</a></p>
+              <h3 class="card-title">Inducci&oacute;n</h3>
+              <p style="text-align:justify;">Admin Panel es una aplicaci&oacute;n web administrativa e intuitiva que permite realizar gestiones generales para el Departamento de Recursos Humanos y especialmente, para llevar un control sobre las operaciones de pago de sueldo a los empleados de la empresa.</p>
+              <p style="text-align:justify;">Para comenzar a trabajar de forma eficiente y adecuada debe:</p>
+              <ol>
+                <li>Crear los departamentos cargos que existan en su empresa.</li>
+                <li>Registrar a los empleados actuales con sus datos correspondientes.</li>
+                <li>Asignar la relacion de los empleados con sus respectivos cargos y departamentos.</li>
+                <li>Crear usuarios para los empleados que sean convenientes. (Opcional)</li>
+                <li>Crear o modificar las planillas de pago predefinidas para cada cargo existente.</li>
+                <li>Registrar los nuevos pagos que se han realizado.</li>
+                <li>Revisar el historial de pagos para comprobar que todo va correctamente.</li>
+              </ol>
             </div>
           </div>
           <div class="col-md-6">
             <div class="card">
-              <h3 class="card-title">Compatibility with frameworks</h3>
-              <p>This theme is not built for a specific framework or technology like Angular or React etc. But due to it's modular nature it's very easy to incorporate it into any front-end or back-end framework like Angular, React or Laravel.</p>
-              <p>Go to <a href="http://pratikborsadiya.in/blog/vali-admin" target="_blank">documentation</a> for more details about integrating this theme with various frameworks.</p>
-              <p>The source code is available on GitHub. If anything is missing or weird please report it as an issue on <a href="https://github.com/pratikborsadiya/vali-admin" target="_blank">GitHub</a>. If you want to contribute to this theme pull requests are always welcome.</p>
+              <h3 class="card-title">Obtener Admin Panel</h3>
+              <p>Hasta ahora Admin Panel se puede encontrar de forma gratuita bajo la licencia MIT.</p>
+              <p>Si desea obtener la versi&oacute;n actual o su codigo fuente, puede encontrarla en <a href="https://github.com/Kunai11/municipalidad" target="_blank">GitHub</a> o si desea contribuir en el desarrollo de Admin Panel realice un Pull Request en la direcci&oacute;n del repositorio.</p>
+              <p>Si ha tenido un problema, por favor, env&iacute;elo como un issue a <a href="https://github.com/Kunai11/municipalidad" target="_blank">GitHub</a>.</p>
+              <p class="mt-40 mb-20"><a class="btn btn-primary icon-btn mr-10" href="../en-construccion/" target="_blank"><i class="fa fa-file"></i>Documentaci&oacute;n</a><a class="btn btn-info icon-btn mr-10" href="https://github.com/Kunai11/municipalidad" target="_blank"><i class="fa fa-github"></i>GitHub</a><a class="btn btn-success icon-btn" href="https://github.com/Kunai11/municipalidad/archive/master.zip" target="_blank"><i class="fa fa-download"></i>Download</a></p>
             </div>
           </div>
         </div>
