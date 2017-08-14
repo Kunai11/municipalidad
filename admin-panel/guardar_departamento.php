ca32568e0e34acc687a04ff32fc7c7274f2a837e
Kunai11 <carlomeza11.cm@gmail.com>
2017-08-14 03:38:59 -0600

    Guardar departamento
    Modulo para guardar datos de departamento en base de datos

---
+++ admin-panel/guardar_departamento.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+    if (isset($_SESSION['username'])&&($_SESSION['rank'])) {
+
+        include('../cn/bdconexion.php');
+        $codigo_depto = $_POST['codigo_depto'];
+        $nombre = $_POST['nombre_depto'];
+        
+        
+
+        $queryVerificar = mysqli_query($db, "SELECT COUNT(*) as Existe FROM departamentos WHERE Cod_Dep='$codigo_depto'") or die(mysqli_error());
+        $rowExiste=mysqli_fetch_array($queryVerificar);
+        if ($rowExiste['Existe']==0) {
+            $queryGuardar = mysqli_query($db, "INSERT INTO departamentos (Cod_Dep, Nom_Dep) VALUES ('$codigo_depto', '$nombre') ") or die(mysqli_error());
+            echo 'Guardado';
+        } 
+        if ($rowExiste['Existe']==1) {
+            #echo 'Ya existe';
+        }
+            
+    } else {
+        header('location: page_denegado.php');
+    }
+?>
---
+++ admin-panel/js/tips/departameto_acciones.js
@@ -0,0 +1,86 @@
+$(document).ready(function () {
+	$('#agregar').click(function () {
+		var codigo_depto=$('#codigo_depto').val();
+		var nombre_depto=$('#nombre_depto').val();
+		
+
+		if (codigo_depto=='') {
+			$("#codigo_depto").attr('required',true);
+			document.getElementById("codigo_depto").style.border="2px solid #a94442";
+			document.getElementById("codigo_depto").focus();
+			return false;
+		} else {
+			$("#codigo_depto").attr('required',false);
+			document.getElementById("codigo_depto").style.border="2px solid #3c763d";
+		}
+
+		if (nombre_depto=='') {
+			$("#nombre_depto").attr('required',true);
+			document.getElementById("nombre_depto").style.border="2px solid #a94442";
+			document.getElementById("nombre_depto").focus();
+			return false;
+		} else {
+			$("#nombre_depto").attr('required',false);
+			document.getElementById("nombre_depto").style.border="2px solid #3c763d";
+		}
+
+		var data = 'codigo_depto=' + codigo_depto + '&nombre_depto=' + nombre_depto;
+		//alert(data);
+		$.ajax({
+			
+			url: "guardar_departamento.php",
+
+			data: data,
+
+			type: "POST",			
+
+			dataType: "html",
+			
+			//cache: false,
+			
+			//success
+			success: function (data) {
+				//alert(data);
+				
+				if (data) {
+					$.notify({
+						title: "Correcto : ",
+						message: "Datos de empleado se han guardado existosamente!",
+						icon: 'fa fa-check' 
+					},{
+						type: "success"
+					});
+					limpiarTodo();
+				}
+				if (!data) {
+					$.notify({
+						title: "Error : ",
+						message: "Ya existe un empleado con la identidad ingresada",
+						icon: 'fa fa-times' 
+					},{
+						type: "danger"
+					});
+				}
+				
+			},
+
+			error : function(xhr, status) {
+				// alert('Disculpe, existió un problema');
+			},
+
+			complete : function(xhr, status) {
+				// alert('Petic
+				// $.notify({
+				// 		title: "Informacion : ",
+				// 		message: "Petición realizada!",
+				// 		icon: 'fa fa-check' 
+				// 	},{
+				// 		type: "info"
+				// });
+			}
+
+		});
+				
+		return false;
+	});
+});
